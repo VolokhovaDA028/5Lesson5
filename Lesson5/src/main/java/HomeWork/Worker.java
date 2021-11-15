b2d5645b5374a9713213c759f9c063395f576269
ваш логин на гитхабе < ,    >
2021-11-15 20:21:32 +1200

    Method

--- Lesson5/src/main/java/HomeWork/Worker.java
+++ Lesson5/src/main/java/HomeWork/Worker.java
@@ -1,21 +1,28 @@
 package HomeWork;
 
+import java.text.MessageFormat;
+
 public class Worker {
     // Объекты класса
      String fullName;
      String position;
      String email;
-     String phoneNamber;
+     String phoneNumber;
      int salary;
      int age;
 
     // Конструктор
-    public Worker(String fullName, String position, String email, String phoneNamber, int salary, int age) {
+    public Worker(String fullName, String position, String email, String phoneNumber, int salary, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
-        this.phoneNamber = phoneNamber;
+        this.phoneNumber = phoneNumber;
         this.salary = salary;
         this.age = age;
+    }
+    // Метод
+    public String infoWorker() {
+        String pattern = "fullName: {0}, position: {1}, email: {2}, phone: {3}, salary: {4}, age: {5}";
+        return MessageFormat.format(pattern, this.fullName, this.position, this.email, this.phoneNumber, this.salary, this.age);
     }
 }
