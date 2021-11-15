2af48b2b388288f82c285bba874b97794801a533
ваш логин на гитхабе < ,    >
2021-11-15 20:00:20 +1200

    NewClass

--- Lesson5/src/main/java/HomeWork/worker.java
+++ Lesson5/src/main/java/HomeWork/worker.java
@@ -1,4 +1,12 @@
 package HomeWork;
 
+
+//ФИО, должность, email, телефон, зарплата, возраст.
 public class worker {
+    String fullName;
+    String position;
+    String email;
+    String phoneNumber;
+    int salary;
+    int age;
 }
