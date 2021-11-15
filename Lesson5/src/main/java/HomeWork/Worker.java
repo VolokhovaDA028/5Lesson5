d032c4009ed2c8766ac3d9fbfdb6e0e0f42b4b33
ваш логин на гитхабе < ,    >
2021-11-15 20:32:47 +1200

    Workers

--- Lesson5/src/main/java/HomeWork/Worker.java
+++ Lesson5/src/main/java/HomeWork/Worker.java
@@ -3,6 +3,15 @@ package HomeWork;
 import java.text.MessageFormat;
 
 public class Worker {
+
+    public static void main(String[] args) {
+        Worker[] workers = new Worker[5];
+        workers[0] = new Worker("Borisov Evgeny", "Programmer", "borisov@mail.com", "89146665334",100000,47);
+        workers[1] = new Worker("Bylka Semen", "Designer", "petrov@mail.com", "89363376437",80000,27);
+        workers[2] = new Worker("Vinni Pyh", "Architect", "sidorov@mail.com", "89684543257",60000,55);
+        workers[3] = new Worker("Lubimov Vacily", "Document expert", "pupkina@mail.com", "89167879898",50000,31);
+        workers[4] = new Worker("Kluev Jora", "Engineer", "juk@mail.com", "89246675713",30000,40);
+    }
     // Объекты класса
      String fullName;
      String position;
