a2219c092410980da221ec9524bf3321257c193a
ваш логин на гитхабе < ,    >
2021-11-15 20:11:58 +1200

    Constructor

---
+++ Lesson5/src/main/java/HomeWork/Worker.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+public class Worker {
+    // Объекты класса
+     String fullName;
+     String position;
+     String email;
+     String phoneNamber;
+     int salary;
+     int age;
+
+    // Конструктор
+    public Worker(String fullName, String position, String email, String phoneNamber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNamber = phoneNamber;
+        this.salary = salary;
+        this.age = age;
+    }
+}
